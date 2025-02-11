2e1b4e2fb221fac50facf766bc32b7ebd77bd2f0
Serj <popp.sergiu@gmail.com>
2025-02-11 09:59:36 +0200

    Working stable version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+import './ui/global.css';
+// import { inter } from '@/app/ui/fonts';
+import { inter } from './ui/fonts';
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import AcmeLogo from "@/app/ui/acme-logo";
+// import AcmeLogo from "@/app/ui/acme-logo";
+import AcmeLogo from "./ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { lusitana } from "@/app/ui/fonts";
+// import { lusitana } from "@/app/ui/fonts";
+import { lusitana } from "./ui/fonts";
 import Image from "next/image";
 // import styles from '@/app/ui/home.module.css';
 
